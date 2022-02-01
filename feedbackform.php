062b3183122c97ffd37f92830813c31b834e7384
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-02-01 13:02:55 +0530

    Create feedbackform.php

---
+++ feedbackform.php
@@ -0,0 +1,118 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  <?php
+if ($_SERVER['REQUEST_METHOD']) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $desc = $_POST['desc'];
+  
+
+// sumbit to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="contacts";
+
+  // create a connection
+  $conn = mysqli_connect($servername, $username, $password,$database);
+
+  // die if connection was not successful
+  if (!$conn) {
+    // die("sorry we failed to connect: " . mysqli_connect_error());
+    echo "fail";
+  } else {
+    echo "connection is done";
+  }
+//sql query to be inserted
+$sql="INSERT INTO `contact us` (`sno`, `name`, `email`, `consern`, `date`) VALUES ('', '$name', '$email', '$desc', current_timestamp())";
+$result=mysqli_query($conn,$sql);
+
+if($result){
+  echo '<div class="alert alert-success" role="alert">
+  successfully done</div>';
+}
+else{
+  echo '<div class="alert alert-danger" role="alert">
+  fail</div>';
+}
+}
+?>
+<div class="container">
+<form action="/soham/indexboot.php" method="post">
+  <div class="mb-3">
+    <label for="name" class="form-label">NAME</label>
+    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name">
+  </div>
+  <div class="mb-3">
+    <label for="Email1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+  </div>
+  <div class="mb-3">
+    <label for="desc" class="form-label">DESCRIPTION</label>
+    <textarea class="form-control" id="desc" name="desc" cols="30" rows="6"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form></div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
+
+  
